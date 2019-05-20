3e7540dde5fbc77b09010b9d8b3c9682cb27863b
Kavi <42409843+Kavirthan@users.noreply.github.com>
2019-05-20 09:44:54 +0200

    Maquette

--- VMV/VMV/VMV/MainPage.xaml.cs
+++ VMV/VMV/VMV/MainPage.xaml.cs
@@ -9,9 +9,32 @@ namespace VMV
 {
     public partial class MainPage : ContentPage
     {
+        private Boolean Start;
+        public Boolean start
+        {
+            get { return Start; }
+            set { Start = value;  }
+        }
+
         public MainPage()
         {
             InitializeComponent();
+
+        }
+
+        private async void Connexion_Clicked(object sender, EventArgs e)
+        {
+            // await DisplayAlert(Start.ToString(), "ok", "ok");
+
+
+            await Navigation.PushModalAsync(new Start_Partie());
+
+            
+            if (Start)
+            {
+               // await Navigation.PushAsync(new Score());
+            }
+
         }
     }
 }
---
+++ VMV/VMV/VMV/Parametre.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VMV
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Parametre : ContentPage
+	{
+		public Parametre ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
