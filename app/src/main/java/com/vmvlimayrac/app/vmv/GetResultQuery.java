1397607b23afe35c5d8a484e2ce8eaaa88c3f8bb
Kavi <42409843+Kavirthan@users.noreply.github.com>
2019-07-04 15:34:28 +0200

    a

--- app/src/main/java/com/vmvlimayrac/app/vmv/GetResultQuery.java
+++
@@ -1,9 +0,0 @@
-package com.vmvlimayrac.app.vmv;
-
-import com.google.android.gms.maps.GoogleMap;
-
-import org.json.JSONArray;
-
-public interface GetResultQuery {
-    public void onTaskComplete(JSONArray result,final GoogleMap googleMap );
-}
