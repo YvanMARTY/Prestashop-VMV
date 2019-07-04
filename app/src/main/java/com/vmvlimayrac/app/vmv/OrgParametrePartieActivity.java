5e97cb3c25453851ebf48ca8640c9a6f3315ddfb
Kavi <42409843+Kavirthan@users.noreply.github.com>
2019-07-04 15:42:53 +0200

    Bug Fix

---
+++ app/src/main/java/com/vmvlimayrac/app/vmv/OrgParametrePartieActivity.java
@@ -0,0 +1,27 @@
+package com.vmvlimayrac.app.vmv;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class OrgParametrePartieActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_org_parametre_partie);
+
+        getSupportActionBar().hide();
+
+        Button btn_back = (Button) findViewById(R.id.back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vue) {
+                finish();
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/vmvlimayrac/app/vmv/TabFragment2.java
@@ -0,0 +1,36 @@
+package com.vmvlimayrac.app.vmv;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class TabFragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.tab_fragment_2, container, false);
+
+        Button button = (Button) view.findViewById(R.id.btn_organisateur_connexion);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Toast.makeText(getActivity(), "This is a message!", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(getActivity(), OrgInfoPartieActivity.class);
+                ((MainActivity) getActivity()).startActivity(intent);
+
+
+
+            }
+        });
+
+        return view;
+    }
+
+}
