8a9cbb2365b5278471b96be863e3d3f2eecfa4d2
Adrien <adrien.carre@limayrac.fr>
2019-07-18 00:04:09 +0200

    Merge du code.

--- modules/moduleparcours/controllers/admin/AdminCreationController.php
+++ modules/moduleparcours/controllers/admin/AdminCreationController.php
@@ -18,8 +18,35 @@ class AdminCreationController extends ModuleAdminController
 
         parent::__construct();
 
+        $pointsArray = array();
+
+        /* $sql = 'SELECT * FROM '._DB_PREFIX_.'points';
+        if ($results = Db::getInstance()->ExecuteS($sql))
+        $var=0;
+        foreach ($results as $row) {
+            $pointsArray[$var]=$row;
+            $var++; 
+        } */
+
+        /* ASSIGNER L'ARRAY AU SMARTY */
+        /* $this->context->smarty->assign('points', $pointsArray); */
+
         //$this->setTemplate('module:moduleparcours/views/templates/admin/creation.tpl');
         $this->setTemplate('creation.tpl');
+
+        
     }
 
-}
+    //$cmd = $_GET["cmd"];
+    //$host = "localhost";
+    //$db = "db329628_vmv";
+    
+
+    /*if($conn)
+    {
+        foreach($conn->query('SELECT * FROM Points') as $row) 
+        {
+            echo $row['pts_nom'].' '.$row['pts_id']; //etc...
+        }
+    }*/
+}?>
